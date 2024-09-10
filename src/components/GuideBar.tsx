83c79f74da0b46f454f0d3c85483f0910ba8e137
joshuaVisagie <joshvisagie@gmail.com>
2024-09-10 23:12:43 +0200

    feat: change Guide Bar

--- src/components/GuideBar.tsx
+++ src/components/GuideBar.tsx
@@ -1,24 +1,77 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import * as React from "react";
+import FormatAlignLeftIcon from "@mui/icons-material/FormatAlignLeft";
+import FormatAlignCenterIcon from "@mui/icons-material/FormatAlignCenter";
+import FormatAlignRightIcon from "@mui/icons-material/FormatAlignRight";
+import FormatAlignJustifyIcon from "@mui/icons-material/FormatAlignJustify";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import { useState , useEffect} from "react";
 
-export default function DenseAppBar() {
+const GuideButton = (props) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const handleExpand = () => {
+    setExpanded(true);
+  };
+
+  const handleShrink = () => {
+    if (props.currentNav == props.buttonValue) {
+      return;
+    } else {
+      setExpanded(false);
+    }
+  };
+
+  useEffect(() => {
+    handleShrink()
+    }
+  , [props.currentNav])
+  
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="sticky">
-        <Toolbar variant="dense">
-          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" color="inherit" component="div">
-            Photos
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <ToggleButton
+      value={props.buttonValue}
+      aria-label={props.buttonLabel}
+      onMouseEnter={handleExpand}
+      onMouseLeave={handleShrink}
+      sx={{
+        borderRadius:"12px",
+        margin:"20px",
+        border:"0"
+      }}
+    >
+      {expanded ? <div>{props.buttonValue}</div> : <FormatAlignJustifyIcon />}
+    </ToggleButton>
+  );
+};
+
+export default function GuideBar() {
+  const [nav, setNav] = React.useState<string | null>("home");
+
+  const handleNav = (
+    event: React.MouseEvent<HTMLElement>,
+    newNav: string | null
+  ) => {
+    console.log(newNav);
+    setNav(newNav);
+  };
+
+  return (
+    <ToggleButtonGroup
+      value={nav}
+      exclusive
+      onChange={handleNav}
+      aria-label='navigation'
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
+      <GuideButton buttonValue='home' buttonLabel='home' currentNav={nav} />
+      <GuideButton buttonValue='liked' buttonLabel='home' currentNav={nav} />
+      <GuideButton buttonValue='search' buttonLabel='home' currentNav={nav} />
+      <GuideButton buttonValue='settings' buttonLabel='home' currentNav={nav} />
+      <GuideButton buttonValue='user' buttonLabel='home' currentNav={nav} />
+    </ToggleButtonGroup>
   );
 }
